a7dcd428166673f6d28ccf09e118e2d40f326ff0
Chloekkk <skdusdl8804@gmail.com>
2023-03-21 12:12:40 +0900

    ✨ feat: Separate ErrorHandler.hpp and ErrorHandler.cpp

--- include/handler/ErrorHandler.hpp
+++ include/handler/ErrorHandler.hpp
@@ -2,10 +2,10 @@
 #define ERRORHANDLER_HPP
 
 #include <string>
-#include "entity/Client.hpp"
-#include "core/Parser.hpp"
 
 namespace ft {
+
+class Client;
 
 class ErrorHandler {
     enum e_err_code {
@@ -47,24 +47,6 @@ class ErrorHandler {
     static void handleError(std::exception &e, Client *src);
     static std::string getErrorMessage(e_err_code code);
 };
-
-const std::string ErrorHandler::servername = "eyeRsee.local";
-
-const std::string ErrorHandler::ERR_UNKNOWNCOMMAND_MSG = "Unknown command";
-const std::string ErrorHandler::ERR_NEEDMOREPARAMS_MSG = "Not enough parameters";
-
-const std::string ErrorHandler::ERR_ALREADYREGISTERED_MSG = "You may not reregister";
-const std::string ErrorHandler::ERR_NONICKNAMEGIVEN_MSG = "No nickname given";
-const std::string ErrorHandler::ERR_ERRONEUSNICKNAME_MSG = "Erroneus nickname";
-const std::string ErrorHandler::ERR_NICKNAMEINUSE_MSG = "Nickname is already in use";
-const std::string ErrorHandler::ERR_NOTONCHANNEL_MSG = "You're not on that channel";
-const std::string ErrorHandler::ERR_USERONCHANNEL_MSG = "is already on channel";
-const std::string ErrorHandler::ERR_UNKNOWNMODE_MSG = "is unknown mode char to me";
-const std::string ErrorHandler::ERR_INVITEONLYCHAN_MSG = "Cannot join channel (+i)";
-const std::string ErrorHandler::ERR_CHANOPRIVSNEEDED_MSG = "You're not channel operator";
-const std::string ErrorHandler::ERR_NOSUCHNICK_MSG = "No such nick/channel";
-const std::string ErrorHandler::ERR_NOSUCHCHANNEL_MSG = "No such channel";
-const std::string ErrorHandler::ERR_BADCHANMASK_MSG = "Invalid channel name";
 
 }
 
